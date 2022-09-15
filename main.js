eb074e45aa48b845e18067d97584b4b6d1620efc
Mohammad Farhan <105407108+maverick-farhan@users.noreply.github.com>
2022-09-15 20:09:28 +0530

    Commit
    Commit It

---
+++ main.js
@@ -0,0 +1,11 @@
+VANTA.BIRDS({
+  el: "#vantajs",
+  /*: more options (Uncomment) */
+  //birdSize: 1.5,
+  wingSpan: 21.0,
+  //speedLimit: 4.00,
+  //separation: 32.00,
+  //alignment: 33.00,
+  //cohesion: 33.00,
+  //quantity: 2.00
+});
